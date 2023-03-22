0a0babc7e70c7423b5bd9a02e3d431f95431c4b6
dhairyam2002 <dhairyam2002@gmail.com>
2023-03-22 18:28:13 +0530

    initial setup with sockets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, GestureResponderEvent, StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io("http://192.168.1.4:3001")
 
 export default function App() {
+  
+  async function handlePress(title: string) {
+    console.log(title)
+    AsyncStorage.setItem("id", title);
+    try {
+      const data = await fetch('http://192.168.1.4:3001')
+      const res = await data.json();
+      socket.connect();
+      socket.emit("chat")
+      // console.log(res);
+    } catch (error) {
+      console.log(error)
+    }
+    
+  }
+
+  useEffect(() => {
+    console.log('rendred');
+    
+    socket.connect()
+    socket.on("connection" , () => console.log('connected'));
+    console.log('done')
+    socket.emit('title');
+    
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Press this for 2 id' onPress={() => handlePress('2')} />
+      <Button title='Press this for 3 id' onPress={() => handlePress('3')} />
     </View>
   );
 }
